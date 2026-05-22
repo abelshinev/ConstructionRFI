f5ba16cc070955ed84e03638015a9f2a291374e8
omi <sowmithbasak13@gmail.com>
2026-05-22 23:46:32 +0530

    feat: added logs

--- services/ingestion/storage.py
+++ services/ingestion/storage.py
@@ -17,5 +17,12 @@ TEMP_DIR.mkdir(parents=True, exist_ok=True)
 
 # save the file according to destination directory
 def save_temp_file(upload_file, destination: Path):
+    """Used to save the file in destination directory
+    Args:
+        upload_file: File uploaded -> treated as an object
+        destination: Path to the designated directory where the file has to be stored
+    Returns:
+       None
+    """
     with open(destination, "wb") as buffer:
         shutil.copyfileobj(upload_file.file, buffer)
