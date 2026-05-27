8572a8fe0693aebbff4d56bc0e1089ba4bcca984
Sowmith <143250328+showmyth@users.noreply.github.com>
2026-05-27 19:33:29 +0530

    Feat:  OCR Logic + OCR pipeline addition in Celery Worker Queue (#5)
    * fix: add more secrets
    
    * feat: added ocr logic + ocr pipeline for celery worker

--- apps/worker/main.py
+++ apps/worker/main.py
@@ -1,8 +1,16 @@
 import os
 import asyncio
+
+from pathlib import Path
 from celery import Celery
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from services.ocr.recognition import extract_from_media
+from packages.shared_schemas import asset
+
 from storage.database.connect import AsyncSessionLocal
-from storage.database.models import Asset, ProcessingStatus
+from storage.database.models import Asset, ExtractedContent, ProcessingStatus
 import logging
 
 REDIS_URL = os.getenv("REDIS_URL", "redis://redis:6379/0")
@@ -36,22 +44,57 @@ async def process_asset_async(asset_id: str):
             asset.processing_status = ProcessingStatus.PROCESSING
             await db.commit()
             
-            # Simulate processing time
-            print(f"Processing asset {asset.original_filename} at {asset.stored_path}...")            
-            await asyncio.sleep(3)  # <---  simulting ocr or vision work
-            # ^^^ Replace with actual processing logic 
+            # # Simulate processing time
+            # print(f"Processing asset {asset.original_filename} at {asset.stored_path}...")            
+            # await asyncio.sleep(3)  # <---  simulting ocr or vision work
+            # # ^^^ Replace with actual processing logic 
 
-            # Update status to READY
+            # Adding OCR pipeline
+            asset_path = Path(asset.stored_path)
+            extracted_content = None
+
+            # Apply OCR based on content
+            if asset.content_type.startswith("image/"):
+                logger.info(f"Extracting text from Image: {asset.original_filename}")
+                extracted_content = await extract_from_media(asset_path)
+
+            elif asset.content_type.startswith("pdf/"):
+                logger.info(f"Extracting text from PDF: {asset.original_filename}")
+                extracted_content = await extract_from_media(asset_path)
+            
+            # Store extracted content in new DB table
+            if extracted_content:
+                new_extracted = ExtractedContent(
+                    asset_id=asset.id,
+                    extracted_text=extracted_content.get("text"),
+                    content_type=extracted_content.get("source"),  # 'tesseract' or 'pdfplumber'
+                    extraction_metadata={
+                        "pages": extracted_content.get("pages"),
+                        "source": extracted_content.get("source"),
+                        **extracted_content.get("metadata", {})
+                    }
+                )
+                db.add(new_extracted)
+                await db.commit()
+                logger.info(f"Extracted content stored for asset {asset_id}")
+            
+            # Update Status to Ready
+
+            logger.info(f"Extraction done! {extracted_content}")
             asset.processing_status = ProcessingStatus.READY
             await db.commit()
-            print(f"Asset {asset_id} processed successfully.")
+
+            # # Update status to READY
+            # asset.processing_status = ProcessingStatus.READY
+            # await db.commit()
+            # print(f"Asset {asset_id} processed successfully.")
         
-        except Exception as e:
+        except Exception as err:
             await db.rollback()
-            print(f"Error processing asset {asset_id}: {e}")
+            print(f"Error processing asset {asset_id}: {err}")
             asset.processing_status = ProcessingStatus.FAILED
             await db.commit()
-            raise e
+            raise 
 
 # --- SYNC CELERY TASK WRAPPER ---
 @celery_app.task(bind=True, max_retries=3)
